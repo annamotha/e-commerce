0941b05dc85d2a237c94a6c5459bbb2c3065d2ec
Anna Motha <annaclaraamotha@gmail.com>
2024-10-24 09:28:21 -0300

    menu

--- script.js
+++ script.js
@@ -25,14 +25,8 @@ dots.forEach((dot, index) => {
 });
 
 
-
 //menu links
 document.getElementById('menuToggle').addEventListener('click', function() {
     var menu = document.getElementById('menu');
-    
-    if (menu.classList.contains('active')) {
-        menu.classList.remove('active');
-    } else {
-        menu.classList.add('active');
-    }
+    menu.classList.toggle('active');
 });
